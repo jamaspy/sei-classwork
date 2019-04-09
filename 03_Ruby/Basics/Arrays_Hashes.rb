8ad967338aee68c4a5dd7211ca15ead96d818448
James Aspinall <jamesaspinall@Jamess-MacBook.local>
2019-04-09 15:33:45 +1000

    RUBY: Basics Day 2

---
+++ 03_Ruby/Basics/Arrays_Hashes.rb
@@ -0,0 +1,18 @@
+
+users = {
+  "Jonathan" => {
+    :twitter => "tronathan",
+    :favorite_numbers => [12, 42, 75],
+  },
+  "Erik" => {
+    :twitter => "sferik",
+    :favorite_numbers => [8, 12, 24],
+  },
+  "Anil" => {
+    :twitter => "bridgpal",
+    :favorite_numbers => [12, 14, 85],
+  },
+}
+
+users["James"] = {:twitter => "thepeopletester", :favorite_number => []}
+p users["Anil"][:favorite_numbers]
---
+++ 03_Ruby/Basics/hello.rb
@@ -0,0 +1,6 @@
+
+random_number = rand(100)
+
+
+newArray = Array.new(5) { rand(1..37)}
+p newArray
